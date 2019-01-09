c903828b2582c4481cf9021a413c0f4c888efcac
Miralles91 <43479445+Miralles91@users.noreply.github.com>
2019-01-09 16:34:55 +0100

    Actualizacion

--- ServerTcp/ServerForm.cs
+++ ServerTcp/ServerForm.cs
@@ -36,7 +36,7 @@ namespace ServerTcp
             try
             {
                 Int32 port = 5000;
-                IPAddress localAddr = IPAddress.Parse("172.17.20.73");
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
                 Listener = new TcpListener(localAddr, port);
                 Listener.Start();
                 Byte[] bytes = new Byte[1024];
